304369e80d881cef62dce94086fe658a5d583ce1
AlexLuthor135 <ettariel123@gmail.com>
2025-02-15 13:02:46 +0100

    Completed project with merged k8s folder

--- p3/scripts/set_dev.sh
+++ p3/scripts/set_dev.sh
@@ -4,9 +4,9 @@ set -e
 export ARGOCD_SERVER=localhost:8081
 argocd login $ARGOCD_SERVER --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode) --insecure
 kubectl create namespace dev
-argocd repo add https://github.com/AlexLuthor135/alappas.git
+argocd repo add https://github.com/AlexLuthor135/Inception_of_Things.git
 argocd app create my-app \
-  --repo https://github.com/AlexLuthor135/alappas.git \
+  --repo https://github.com/AlexLuthor135/Inception_of_Things.git \
   --path k8s \
   --dest-server https://kubernetes.default.svc \
   --dest-namespace dev
